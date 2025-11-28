c5d9d733a2c6616ae314b4542b659b6419957dcc
LEVAMA2233 <leopoldotrimex2233@gmail.com>
2025-11-28 17:30:05 -0600

    Ejercicio weatherForecast

--- js-03-control_flow/src/js/main.js
+++ js-03-control_flow/src/js/main.js
@@ -1,3 +1,4 @@
+
 const temperatura=(t=18)=>
     {
         if (t>25 && t<=34)
@@ -19,3 +20,23 @@ const temperatura=(t=18)=>
     };
     
 console.log(temperatura(52));
+
+
+const weatherForecast=weathercode=>{
+    let respuesta;
+    switch(weathercode)
+    {
+        case 0:
+            respuesta="Clear Sky";
+            break;
+        case 1,2,3:
+            respuesta="Mainly clear, partly cloudy, and overcasr";
+            break;
+        case 45,48:
+            respuesta="Fog and deposting rime frog";
+            break;
+    }
+    return respuesta;
+}
+
+console.log(weatherForecast(3));
