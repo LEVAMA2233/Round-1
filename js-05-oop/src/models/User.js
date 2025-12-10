fe93605e066bcffcbdea2d991f6c0b1957adfa0f
LEVAMA2233 <leopoldotrimex2233@gmail.com>
2025-12-10 12:31:48 -0600

    Primera interacción con vite

---
+++ js-05-oop/src/models/User.js
@@ -0,0 +1,11 @@
+
+class Usuario{
+    constructor(firstName,lastName,age,correoElectronico){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+        this.email=correoElectronico;
+    }
+}
+
+export{Usuario}
